59b83fc930dc2553c68368607868c68167de80f5
RICHARD EKYEM <ekyemrichard@gmail.com>
2023-03-02 12:48:14 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
  * Return: if str1 < str2, the negative difference of the first unmatched characters.
- * if str1 == str2, 0
+ * if str1 == str2, 0.
  * if str1 > str2, the positive difference of the first unmatched characters.
  *
  */
